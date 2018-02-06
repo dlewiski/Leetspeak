194d7b68f9b2baf80244869b992fdef87f1637c2
David Lewis and Kaila Goff <kaila@>
2018-02-05 16:24:09 -0800

    Work on z and s functionality

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -1,5 +1,35 @@
 class String
   def leetspeak()
-    self
+    leet_array = []
+    new_words_array = []
+    words_array = self.split()
+    words_array.each do |word|
+      letters_array = word.split('')
+      letters_array.each do |letter|
+        if letter == "e"
+          letter = 3
+          new_words_array.push(letter)
+        elsif letter == "o"
+          letter = 0
+          new_words_array.push(letter)
+        elsif letter == "I"
+          letter = 1
+          new_words_array.push(letter)
+        elsif (letter == "s") && (letters_array[letters_array.index(letter) - 1] != " ")
+          letter = "z"
+          new_words_array.push(letter)
+        else
+          new_words_array.push(letter)
+        end
+
+      end
+    leet_array = new_words_array.join()
+    binding.pry
+    end
+  leet_array
   end
 end
+
+
+# leet_array.push(new_words_array)
+# leet_array.join('')
--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -1,5 +1,6 @@
 require('rspec')
 require('leetspeak')
+require('pry')
 
 describe('String#leetspeak') do
   it('returns a string as is when no Leetspeak rules apply') do
@@ -10,5 +11,21 @@ end
 describe('String#leetspeak') do
   it('replaces every "e" in a string with a "3"') do
     expect("elephant".leetspeak).to(eq("3l3phant"))
+  end
+
+  it('replaces every "o" in a string with a "0"') do
+    expect("boo boo".leetspeak).to(eq("b00 b00"))
+  end
+
+  it('replaces every "I" in a string with a "1"') do
+    expect("I like Ike".leetspeak).to(eq("1 lik3 1k3"))
+  end
+
+  it('replaces every "s" in a string with a "z"') do
+   expect("roses".leetspeak).to(eq("r0z3z"))
+ end
+
+ it('does NOT replace the first letter when it is an s') do
+    expect("sassafrass sass".leetspeak).to(eq("sazzafrazz sazz"))
   end
 end
